56bd79a8229aba3ec2acb398881cdf3b77f939ec
RodrigoA15 <rodri150306@gmail.com>
2026-07-23 15:46:10 -0500

    Add: Validation xlsx file

--- src/controllers/qits.controller.js
+++ src/controllers/qits.controller.js
@@ -1,22 +1,58 @@
+import multer from "multer";
+import fs from "node:fs/promises";
+import { join } from "node:path";
 import qitsScraperService from "../services/qits.scraper.service.js";
+import readFileService from "../services/readFile.service.js";
 
 export const scrapingQITSV2 = async (req, res) => {
-  const { data } = req.body;
-
-  if (!data || !Array.isArray(data)) {
-    return res.status(400).json({
-      message:
-        "Formato de datos inválido. Se espera un array en la propiedad 'data'.",
-    });
-  }
-
   try {
-    const result = await qitsScraperService.runProcess(data);
+    const path = multer({ dest: "src/data/" });
+    const storage = multer.diskStorage({
+      destination: (req, file, cb) => {
+        cb(null, "src/data/");
+      },
 
-    return res.status(200).json({
-      message: "Scraping completado",
-      total: data.length,
-      counts: result.counts,
+      filename: (req, file, cb) => {
+        cb(null, file.originalname);
+      },
+    });
+
+    const upload = multer({
+      storage: storage,
+      dest: path,
+      fileFilter: (req, file, cb) => {
+        const filetypes = /xlsx|xls|sheet/;
+        const mimetype = filetypes.test(file.mimetype);
+        if (mimetype) {
+          return cb(null, true);
+        }
+        cb(new Error("Archivo no válido, solo se permiten archivos Excel"));
+      },
+    });
+
+    upload.single("GUIAS_COBRO")(req, res, async (err) => {
+      if (err) {
+        return res.status(400).json(err.message);
+      }
+
+      if (!req.file) {
+        return res.status(400).json("No se ha enviado ningún archivo");
+      }
+
+      console.log("Archivo creado correctamente");
+
+      const data = await readFileService.readXlsx();
+
+      const result = await qitsScraperService.runProcess(data);
+      const PATH_FILE = await fs.readdir("src/data");
+      const path = join("src/data", PATH_FILE[0]);
+      await fs.unlink(path);
+
+      return res.status(200).json({
+        message: "Scraping completado",
+        total: data.length,
+        counts: result.counts,
+      });
     });
   } catch (error) {
     console.error("Error crítico en el controlador QITS:", error);
